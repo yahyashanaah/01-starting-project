8400ddd73454b32f71665d39b587c6ebbd2ead19
yahyashanaah <yahya.shanaah@gmail.com>
2024-08-29 21:57:39 +0400

    first_step

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,9 +1,44 @@
+// investment.service.ts
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvestmentService {
+  calculateInvestmentResults(
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  ): InvestmentResult[] {
+    const annualData: InvestmentResult[] = [];
+    let investmentValue = initialInvestment;
 
-  constructor() { }
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment,
+        totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
+
+export interface InvestmentResult {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
 }
